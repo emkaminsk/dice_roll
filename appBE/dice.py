dd2cbbb51f75b48259ce998504d7be6d0731ea11
Marcin Kaminski <emkaminsk@gmail.com>
2022-12-11 14:24:09 +0100

    initial commit

---
+++ appBE/dice.py
@@ -0,0 +1,37 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from random import randint
+from urllib.parse import parse_qs, urlparse
+import json
+
+class DiceRollRequestHandler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    # parse the query string from the URL
+    query_string = parse_qs(urlparse(self.path).query)
+    max = int(query_string.get('max', [6])[0])
+
+    # generate a random integer in the range 1 to max
+    roll = randint(1, max)
+
+    # create the response data
+    response_data = {
+        'max': max,
+        'rolls': [roll],
+        'total': roll,
+        'add_info': "Docker"
+    }
+
+    # send a JSON response with the dice roll result
+    self.send_response(200)
+
+    # add the appropriate CORS headers
+    self.send_header('Access-Control-Allow-Origin', '*')
+    self.send_header('Access-Control-Allow-Methods', 'GET')
+    self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+
+    self.send_header('Content-type', 'application/json')
+    self.end_headers()
+    self.wfile.write(bytes(json.dumps(response_data), 'utf-8'))
+
+httpd = HTTPServer(('0.0.0.0', 8081), DiceRollRequestHandler)
+print("Listening on port", httpd.server_port)
+httpd.serve_forever()
